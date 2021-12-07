cf4459ebda8b012fc1264b5ba4ce40ff9446d027
jones6951 <mjones2k12@gmail.com>
2021-12-07 16:34:03 -0500

    first

---
+++ createMeta.sh
@@ -0,0 +1,237 @@
+#!/bin/sh
+
+# Copyright (c) 2021 Synopsys, Inc. All rights reserved worldwide.
+
+for i in "$@"; do
+    case "$i" in
+    --url=*) url="${i#*=}" ;;
+    --token=*) token="${i#*=}" ;;
+    *) ;;
+    esac
+done
+
+if [ -z "$url" ]; then
+    echo "You must specify a URL"
+    echo "Usage: createMeta.sh --url --token"
+    exit 1
+fi
+if [ -z "$token" ]; then
+    echo "You must specify a Personal Access Token"
+    echo "Usage: createMeta.sh --url --token"
+    exit 1
+fi
+
+metadata='[{
+  "name": "Risk",
+  "type": "enum",
+  "values": [
+    {
+      "valueId": "high",
+      "value": "High"
+    },
+    {
+      "valueId": "medium",
+      "value": "Medium"
+    },
+    {
+      "valueId": "low",
+      "value": "Low"
+    }
+  ]
+}]'
+
+result=$(curl -s -X 'POST' "$url/codedx/api/project-fields" -H 'accept: application/json' -H "API-Key: $token" -H 'Content-Type: application/json' -d "$metadata")
+echo "Result = [$result]"
+
+metadata='[
+  {
+    "name": "Application Type",
+    "type": "enum",
+    "values": [
+      {
+        "valueId": "Financial",
+        "value": "Financial"
+      },
+      {
+        "valueId": "DMZ",
+        "value": "DMZ"
+      }
+    ]
+  }
+]'
+result=$(curl -s -X 'POST' "$url/codedx/api/project-fields" -H 'accept: application/json' -H "API-Key: $token" -H 'Content-Type: application/json' -d "$metadata")
+echo "Result = [$result]"
+
+metadata='[
+  {
+    "name": "SOX Compliant",
+    "type": "enum",
+    "values": [
+      {
+        "valueId": "true",
+        "value": "True"
+      },
+      {
+        "valueId": "false",
+        "value": "False"
+      }
+    ]
+  }
+]'
+
+result=$(curl -s -X 'POST' "$url/codedx/api/project-fields" -H 'accept: application/json' -H "API-Key: $token" -H 'Content-Type: application/json' -d "$metadata")
+echo "Result = [$result]"
+
+metadata='[
+  {
+    "name": "Protected Personal Information",
+    "type": "enum",
+    "values": [
+      {
+        "valueId": "true",
+        "value": "True"
+      },
+      {
+        "valueId": "false",
+        "value": "False"
+      }
+    ]
+  }
+]'
+
+result=$(curl -s -X 'POST' "$url/codedx/api/project-fields" -H 'accept: application/json' -H "API-Key: $token" -H 'Content-Type: application/json' -d "$metadata")
+echo "Result = [$result]"
+
+metadata='[
+  {
+    "name": "Material Non-Public Information",
+    "type": "enum",
+    "values": [
+      {
+        "valueId": "true",
+        "value": "True"
+      },
+      {
+        "valueId": "false",
+        "value": "False"
+      }
+    ]
+  }
+]'
+
+result=$(curl -s -X 'POST' "$url/codedx/api/project-fields" -H 'accept: application/json' -H "API-Key: $token" -H 'Content-Type: application/json' -d "$metadata")
+echo "Result = [$result]"
+
+metadata='[
+  {
+    "name": "Classification of Information",
+    "type": "enum",
+    "values": [
+      {
+        "valueId": "Highly_Restricted",
+        "value": "Highly Restricted"
+      },
+      {
+        "valueId": "Restricted",
+        "value": "Restricted"
+      },
+      {
+        "valueId": "Internal",
+        "value": "Internal"
+      },
+      {
+        "valueId": "Public",
+        "value": "Public"
+      }
+    ]
+  }
+]'
+
+result=$(curl -s -X 'POST' "$url/codedx/api/project-fields" -H 'accept: application/json' -H "API-Key: $token" -H 'Content-Type: application/json' -d "$metadata")
+echo "Result = [$result]"
+
+metadata='[
+  {
+    "name": "Customer Facing",
+    "type": "enum",
+    "values": [
+      {
+        "valueId": "true",
+        "value": "True"
+      },
+      {
+        "valueId": "false",
+        "value": "False"
+      }
+    ]
+  }
+]'
+
+result=$(curl -s -X 'POST' "$url/codedx/api/project-fields" -H 'accept: application/json' -H "API-Key: $token" -H 'Content-Type: application/json' -d "$metadata")
+echo "Result = [$result]"
+
+metadata='[
+  {
+    "name": "Externally Facing",
+    "type": "enum",
+    "values": [
+      {
+        "valueId": "true",
+        "value": "True"
+      },
+      {
+        "valueId": "false",
+        "value": "False"
+      }
+    ]
+  }
+]'
+
+result=$(curl -s -X 'POST' "$url/codedx/api/project-fields" -H 'accept: application/json' -H "API-Key: $token" -H 'Content-Type: application/json' -d "$metadata")
+echo "Result = [$result]"
+
+metadata='[
+  {
+    "name": "Asset Tier",
+    "type": "enum",
+    "values": [
+      {
+        "valueId": "Tier 01",
+        "value": "Tier 1"
+      },
+      {
+        "valueId": "Tier 02",
+        "value": "Tier 2"
+      },
+      {
+        "valueId": "Tier 03",
+        "value": "Tier 3"
+      }
+    ]
+  }
+]'
+
+result=$(curl -s -X 'POST' "$url/codedx/api/project-fields" -H 'accept: application/json' -H "API-Key: $token" -H 'Content-Type: application/json' -d "$metadata")
+echo "Result = [$result]"
+
+metadata='[
+  {
+    "name": "Fair Lending",
+    "type": "enum",
+    "values": [
+      {
+        "valueId": "true",
+        "value": "True"
+      },
+      {
+        "valueId": "false",
+        "value": "False"
+      }
+    ]
+  }
+]'
+
+result=$(curl -s -X 'POST' "$url/codedx/api/project-fields" -H 'accept: application/json' -H "API-Key: $token" -H 'Content-Type: application/json' -d "$metadata")
+echo "Result = [$result]"
+
+exit 0
---
+++ getTPI.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+# Copyright (c) 2021 Synopsys, Inc. All rights reserved worldwide.
+
+for i in "$@"; do
+    case "$i" in
+    --url=*) url="${i#*=}" ;;
+    --apikey=*) apikey="${i#*=}" ;;
+    --project=*) project="${i#*=}" ;;
+    *) ;;
+    esac
+done
+
+if [ -z "$url" ]; then
+    echo "You must specify a URL"
+    echo "Usage: getTPI.sh --url --apikey --project"
+    exit 1
+fi
+if [ -z "$apikey" ]; then
+    echo "You must specify an API key"
+    echo "Usage: getTPI.sh --url --apikey --project"
+    exit 1
+fi
+if [ -z "$project" ]; then
+    echo "You must specify a project"
+    echo "Usage: getTPI.sh --url --apikey --project"
+    exit 1
+fi
+
+projectID=$(curl -s -X 'GET' "$url/codedx/api/projects" -H 'accept: application/json' -H "API-Key: $apikey" |jq ".projects[] | select(.name==\"$project\").id")
+if [ -z $projectID ]; then
+    echo "Project not found"
+    exit 1
+fi
+
+metadata=$(curl -s -X 'GET' "$url/codedx/api/projects/$projectID/metadata"  -H 'accept: application/json' -H "API-Key: $apikey")
+
+risk=$(echo $metadata |jq ".[] | select(.name==\"Risk\")".valueId)
+assetType="Application"
+applicationType=$(echo $metadata |jq ".[] | select(.name==\"Application Type\")".valueId)
+applicationName=\"$project\",
+soxFinancial=$(echo $metadata |jq ".[] | select(.name==\"SOX Compliant\")".valueId|sed 's/"//g')
+ppi=$(echo $metadata |jq ".[] | select(.name==\"Protected Personal Information\")".valueId|sed 's/"//g')
+mnpi=$(echo $metadata |jq ".[] | select(.name==\"Material Non-Public Information\")".valueId|sed 's/"//g')
+infoClass=$(echo $metadata |jq ".[] | select(.name==\"Classification of Information\")".valueId)
+customerFacing=$(echo $metadata |jq ".[] | select(.name==\"Customer Facing\")".valueId|sed 's/"//g')
+externallyFacing=$(echo $metadata |jq ".[] | select(.name==\"Externally Facing\")".valueId|sed 's/"//g')
+assetTier=$(echo $metadata |jq ".[] | select(.name==\"Asset Tier\")".valueId)
+fairLending=$(echo $metadata |jq ".[] | select(.name==\"Fair Lending\")".valueId|sed 's/"//g')
+
+echo "{
+  \"assetId\": \"$project\",
+  \"assetType\": \"Application\",
+  \"applicationType\": $applicationType,
+  \"applicationName\": \"$project\",
+  \"applicationBuildName\": \"main-branch\",
+  \"soxFinancial\": $soxFinancial,
+  \"ppi\": $ppi,
+  \"mnpi\": $mnpi,
+  \"infoClass\": $infoClass,
+  \"customerFacing\": $customerFacing,
+  \"externallyFacing\": $externallyFacing,
+  \"assetTier\": $assetTier,
+  \"fairLending\": $fairLending
+}"
+
+exit 0
