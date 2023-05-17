de4d2bcdd658c326a4b838dbafcea2c45abdebeb
jones6951 <mjones2k12@gmail.com>
2023-05-17 13:48:55 -0400

    Updated

--- checkPolicy.sh
+++ checkPolicy.sh
@@ -34,17 +34,24 @@ if [ -z "$apikey" ]; then
     exit 1
 fi
 
+if [ -z "$policy" ]; then
+    policyID=any
+else
+    policyID=$(curl -k -s -X 'GET' "$url/codedx/x/policies" -H 'accept: application/json' -H "API-Key: $apikey"| jq ".[] | select(.name==\"$policy\").id")
+    if [ -z $policyID ]; then
+        echo "Policy not found"
+        exit 1
+    fi
+fi
+
 projectID=$(curl -k -s -X 'GET' "$url/codedx/api/projects" -H 'accept: application/json' -H "API-Key: $apikey" |jq ".projects[] | select(.name==\"$project\").id")
 if [ -z $projectID ]; then
     echo "Project not found"
     exit 1
 fi
-
-branchID=$(curl -k -s -X 'GET' "$url/codedx/x/projects/$projectID/branches" -H 'accept: application/json' -H "API-Key: $apikey" |jq ".[].id")
-
-policyStatus=$(curl -k -s -X 'GET' "$url/codedx/api/projects/$projectID;branchId=$branchID/policies/$policy/build-broken" -H 'accept: application/json' -H "API-Key: $apikey")
-if [ -z $projectID ]; then
-    echo "Policy not found"
+policyStatus=$(curl -k -s -X 'GET' "$url/codedx/x/projects/$projectID;branch=$branch/policies/$policyID/build-broken" -H 'accept: application/json' -H "API-Key: $apikey")
+if [ -z $policyStatus ]; then
+    echo "Error"
     exit 1
 fi
 
